ed300f44bc1515fc7ca45bc5890731fc10f2855d
Hans Peter <latuskati@gmail.com>
2025-02-02 16:57:49 +0100

    arena clear

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-import {world, system, Player, Entity, PlatformType} from "@minecraft/server";
+import {world, system, Player, Entity, PlatformType, TicksPerSecond, BlockVolume, Dimension} from "@minecraft/server";
 
 const admins = [
     "BioTomateDE",
@@ -8,6 +8,23 @@ const admins = [
 ]
 
 const playerNameCharset = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 ()".split("");
+
+
+function isValid(value) {
+    if (value === undefined || typeof value === 'undefined') return false;
+    if (value === null) return false;
+    if (typeof value === 'number' && (isNaN(value) || !isFinite(value))) return false;
+    return true;
+}
+
+
+function isValidPlayer(player) {
+    if (!isValid(player)) return false;
+    if (player.typeId !== "minecraft:player") return false;
+    if (!player.isValid()) return false;
+    return true;
+}
+
 
 function commandifyPlayerName(nameRaw) {
     if (!nameRaw.split("").every(ch => playerNameCharset.includes(ch))) {
@@ -20,9 +37,9 @@ function commandifyPlayerName(nameRaw) {
 }
 
 
-function getObjective(objectiveName, creationDisplayName=null) {
+function getObjective(objectiveName, creationDisplayName = null) {
     let objective = world.scoreboard.getObjective(objectiveName);
-    if (objective !== undefined) {
+    if (isValid(objective)) {
         return objective;
     }
     world.sendMessage(`[§gWARN§r] Creating objective "${objectiveName}" since it didn't exist!`);
@@ -36,7 +53,7 @@ function getObjective(objectiveName, creationDisplayName=null) {
 
 function getScore(objective, player) {
     let score = objective.getScore(player);
-    if (score === undefined) {
+    if (!isValid(score)) {
         score = 0;
     }
     return score;
@@ -62,21 +79,37 @@ function log(...args) {
 }
 
 
-function getKD(player, options={}) {
+function sendSubtitle(message, fadeIn, stay, fadeOut, players = null) {
+    if (players === null) {
+        players = world.getAllPlayers();
+    }
+
+    players.forEach(player => {
+        player.onScreenDisplay.setTitle("§§", {
+            subtitle: message,
+            fadeInDuration: fadeIn,
+            stayDuration: stay,
+            fadeOutDuration: fadeOut
+        });
+    });
+}
+
+
+function getKD(player, options = {}) {
     let scoreboardKills = options['scoreboardKills'];
     let scoreboardDeaths = options['scoreboardDeaths'];
     let kills = options['kills'];
     let deaths = options['deaths'];
 
-    if (kills === undefined) {
-        if (scoreboardKills === undefined) {
+    if (!isValid(kills)) {
+        if (!isValid(scoreboardKills)) {
             scoreboardKills = getObjective("kills");
         }
         kills = getScore(scoreboardKills, player);
     }
 
-    if (deaths === undefined) {
-        if (scoreboardDeaths === undefined) {
+    if (!isValid(deaths)) {
+        if (!isValid(scoreboardDeaths)) {
             scoreboardDeaths = getObjective("deaths");
         }
         deaths = getScore(scoreboardDeaths, player);
@@ -92,26 +125,177 @@ function showKillstreakMessage(player, killstreak) {
     const pluralSuffix = killstreak >= 2 ? "s" : "";
 
     if (killstreak % 5 === 0) {
-        const message = `\n\n\n§b${killstreak}§s Kill${pluralSuffix}!`;
-        player.onScreenDisplay.setTitle("§§", {
-            subtitle: message,
-            fadeInDuration: 0,
-            stayDuration: 35,
-            fadeOutDuration: 7
-        });
+        sendSubtitle(`\n\n\n§b${killstreak}§s Kill${pluralSuffix}!`, 0, 35, 7, [player]);
     } else {
-        const message = `\n\n\n§e${killstreak}§g Kill${pluralSuffix}!`;
-        player.onScreenDisplay.setTitle("§§", {
-            subtitle: message,
-            fadeInDuration: 0,
-            stayDuration: 15,
-            fadeOutDuration: 2
-        });
+        sendSubtitle(`\n\n\n§e${killstreak}§g Kill${pluralSuffix}!`, 0, 15, 2, [player]);
     }
 
     if (killstreak % 10 === 0) {
         world.sendMessage(`§5${player.name}§d is on a killing spree!`);
     }
+}
+
+
+function fillLayers(dimension, from, to, block, blockOptions = null) {
+    // fills every Y level individually. dx*dz should still be under 32769.
+    // `to` should have all coordinates larger than `from`.
+    for (let y = from.y; y <= to.y; y++) {
+        const volume = new BlockVolume(
+            {x: from.x, y: y, z: from.z},
+            {x: to.x, y: y, z: to.z}
+        );
+        dimension.fillBlocks(volume, block, blockOptions);
+    }
+}
+
+
+function clearArena() {
+    world.sendMessage("§aClearing Arena...");
+    const dimension = world.getDimension("overworld");
+
+    const arenaX1 = Math.floor(arenaCenterX - arenaWidthX / 2);
+    const arenaX2 = Math.floor(arenaCenterX + arenaWidthX / 2);
+    const arenaZ1 = Math.floor(arenaCenterZ - arenaWidthZ / 2);
+    const arenaZ2 = Math.floor(arenaCenterZ + arenaWidthZ / 2);
+    const arenaY1 = Math.floor(arenaBottomY);
+    const arenaY2 = Math.floor(arenaBottomY + arenaHeightY);
+
+    const groundFrom = {
+        x: arenaX1 + 1,
+        y: arenaY1,
+        z: arenaX1 + 1
+    }
+    const groundTo = {
+        x: arenaX2 - 1,
+        y: arenaY1,
+        z: arenaZ2 - 1
+    }
+    fillLayers(dimension, groundFrom, groundTo, "minecraft:allow");
+
+    const dirtFrom = {
+        x: arenaX1 + 1,
+        y: arenaY1 + 1,
+        z: arenaX1 + 1
+    }
+    const dirtTo = {
+        x: arenaX2 - 1,
+        y: arenaY1 + 2,
+        z: arenaZ2 - 1
+    }
+    fillLayers(dimension, dirtFrom, dirtTo, "minecraft:dirt");
+
+    const grassFrom = {
+        x: arenaX1 + 1,
+        y: arenaY1 + 3,
+        z: arenaX1 + 1
+    }
+    const grassTo = {
+        x: arenaX2 - 1,
+        y: arenaY1 + 3,
+        z: arenaZ2 - 1
+    }
+    fillLayers(dimension, grassFrom, grassTo, "minecraft:grass_block");
+
+    const airFrom = {
+        x: arenaX1 + 1,
+        y: arenaY1 + 4,
+        z: arenaZ1 + 1
+    }
+    const airTo = {
+        x: arenaX2 - 1,
+        y: arenaY2,    // -0 instead of -1 because the waterlogged barries don't get replaced otherwise
+        z: arenaZ2 - 1
+    }
+    fillLayers(dimension, airFrom, airTo, "minecraft:air");
+    // v this does what the fix above SHOULD'VE done, but for some fucking reason it doesn't work otherwise
+    fillLayers(dimension, airFrom, airTo, "minecraft:air");
+
+    const volumeRoof = new BlockVolume(
+        {
+            x: arenaX1,
+            y: arenaY2,
+            z: arenaZ1
+        },
+        {
+            x: arenaX2,
+            y: arenaY2,
+            z: arenaZ2
+        }
+    );
+    dimension.fillBlocks(volumeRoof, "minecraft:barrier");
+
+    const volumeWallX1 = new BlockVolume(
+        {
+            x: arenaX1,
+            y: arenaY1,
+            z: arenaZ1
+        },
+        {
+            x: arenaX1,
+            y: arenaY2 - 1,
+            z: arenaZ2
+        }
+    );
+    dimension.fillBlocks(volumeWallX1, "minecraft:bedrock");
+
+    const volumeWallX2 = new BlockVolume(
+        {
+            x: arenaX2,
+            y: arenaY1,
+            z: arenaZ1
+        },
+        {
+            x: arenaX2,
+            y: arenaY2 - 1,
+            z: arenaZ2
+        }
+    );
+    dimension.fillBlocks(volumeWallX2, "minecraft:bedrock");
+
+    const volumeWallZ1 = new BlockVolume(
+        {
+            x: arenaX1,
+            y: arenaY1,
+            z: arenaZ1
+        },
+        {
+            x: arenaX2,
+            y: arenaY2 - 1,
+            z: arenaZ1
+        }
+    );
+    dimension.fillBlocks(volumeWallZ1, "minecraft:bedrock");
+
+    const volumeWallZ2 = new BlockVolume(
+        {
+            x: arenaX1,
+            y: arenaY1,
+            z: arenaZ2
+        },
+        {
+            x: arenaX2,
+            y: arenaY2 - 1,
+            z: arenaZ2
+        }
+    );
+    dimension.fillBlocks(volumeWallZ2, "minecraft:bedrock");
+
+
+    // extra air on top of the arena
+    const airOnRoofFrom = {
+        x: arenaX1,
+        y: arenaY2 + 1,
+        z: arenaZ1
+    }
+    const airOnRoofTo = {
+        x: arenaX2,
+        y: arenaY2 + 6,
+        z: arenaZ2
+    }
+    fillLayers(dimension, airOnRoofFrom, airOnRoofTo, "minecraft:air");
+
+    world.sendMessage("§aArena cleared!");
+    sendSubtitle("\n\n§aArena cleared!", 3, 28, 21);
 }
 
 
@@ -132,7 +316,7 @@ world.afterEvents.playerSpawn.subscribe(data => {
 
 // handle kill, death
 world.afterEvents.entityDie.subscribe(data => {
-    if (data.deadEntity.typeId !== "minecraft:player" || !data.deadEntity.isValid()) {
+    if (!isValidPlayer(data.deadEntity)) {
         return;
     }
 
@@ -143,31 +327,26 @@ world.afterEvents.entityDie.subscribe(data => {
     scoreboardDeaths.addScore(data.deadEntity, 1);
     scoreboardKillstreak.setScore(data.deadEntity, 0);
 
-    let attacker;
     // Try to find a killer by finding the player who dealt the most damage to the victim
-    let attackersSorted = Object.entries(playerDamages[data.deadEntity.id])
-        .filter(([attacker, damage]) => damage >= 5)
-        .toSorted(([attacker1, damage1], [attacker2, damage2]) => damage1 > damage2 ? -1 : 1);
+    let attacker;
+    let attackersSorted = [];
 
-    if (attackersSorted.length === 0) {
-        // No attacker with enough damage found, try to give the kill to the "actual" killer
-        if (
-            data.damageSource?.damagingEntity === undefined ||
-            data.damageSource.damagingEntity.typeId !== "minecraft:player" ||
-            !data.damageSource.damagingEntity.isValid()
-        ) {
-            delete playerDamages[data.deadEntity.id];
-            return;
-        }
-        else {
-            attacker = data.damageSource.damagingEntity;
-        }
-    } else {
+    if (data.deadEntity.id in playerDamages) {
+        attackersSorted = Object.entries(playerDamages[data.deadEntity.id])
+            .filter(([attacker, damage]) => damage >= 5)
+            .toSorted(([attacker1, damage1], [attacker2, damage2]) => damage1 > damage2 ? -1 : 1);
+    }
+
+    if (attackersSorted.length > 0) {
         let attackerID = attackersSorted[0][0];
-        attacker = getPlayerByID(attackerID);
+        attacker = getPlayerByID(attackerID);   // can be null
+    }
+    else if (isValidPlayer(data.damageSource?.damagingEntity)) {
+        attacker = data.damageSource.damagingEntity;
     }
 
     delete playerDamages[data.deadEntity.id];
+    if (!isValid(attacker)) return;
 
     const attackerIsInArena = attacker.hasTag("arena");
     scoreboardKills.addScore(attacker, 1);
@@ -186,13 +365,7 @@ world.afterEvents.entityDie.subscribe(data => {
 
 // Keep track of player damages to determine who to award the kill if the death is indirect (fall damage, ender pearl damage, lava, fire, burning)
 world.afterEvents.entityHurt.subscribe(data => {
-    if (
-        data.hurtEntity.typeId !== "minecraft:player" ||
-        !data.hurtEntity.isValid() ||
-        data.damageSource?.damagingEntity === undefined ||
-        data.damageSource.damagingEntity.typeId !== "minecraft:player" ||
-        !data.damageSource.damagingEntity.isValid()
-    ) {
+    if (!isValidPlayer(data.hurtEntity) || !isValidPlayer(data.damageSource?.damagingEntity)) {
         return;
     }
 
@@ -200,8 +373,8 @@ world.afterEvents.entityHurt.subscribe(data => {
     const attacker = data.damageSource.damagingEntity;
     const damageAmount = data.damage;
 
-    // Bow hit confirmation sound
-    if (data.damageSource.damagingProjectile !== undefined) {
+    // Projectile hit confirmation sound
+    if (isValid(data.damageSource?.damagingProjectile)) {
         switch (data.damageSource.damagingProjectile.typeId) {
             case "minecraft:arrow":
                 attacker.playSound("random.orb", {pitch: 0.5});
@@ -216,13 +389,11 @@ world.afterEvents.entityHurt.subscribe(data => {
     }
 
     // Save the cumulative damages for determining kills later
-    if (playerDamages[victim.id] === undefined) {
+    if (!isValid(playerDamages[victim.id])) {
         playerDamages[victim.id] = {attackerID: damageAmount};
-    }
-    else if (playerDamages[victim.id][attacker.id] === undefined) {
+    } else if (!isValid(playerDamages[victim.id][attacker.id])) {
         playerDamages[victim.id][attacker.id] = damageAmount
-    }
-    else {
+    } else {
         playerDamages[victim.id][attacker.id] += damageAmount;
     }
 
@@ -258,9 +429,9 @@ system.runInterval(() => {
 
     allPlayers.forEach((player) => {
         let playtimeTotalTicks = scoreboardPlaytime.getScore(player);
-        playtimeTotalTicks = playtimeTotalTicks === undefined ? 0 : playtimeTotalTicks;
+        playtimeTotalTicks = isValid(playtimeTotalTicks) ? playtimeTotalTicks : 0;
 
-        if (player.typeId !== "minecraft:player") {
+        if (!isValidPlayer(player)) {
             return;
         }
 
@@ -351,6 +522,7 @@ system.runInterval(() => {
 // Kick banned players
 system.runInterval(() => {
     world.getPlayers({tags: ["ban"]}).forEach(player => {
+        log(`§4Kicking banned player §c${player.name}§4.`);
         const banMessage = `§4You have been §mpermanently§4\nbanned from this world, loser!`;
         const command = `kick ${commandifyPlayerName(player.name)} \n${banMessage}`;
         world.getDimension("overworld").runCommand(command);
@@ -358,10 +530,32 @@ system.runInterval(() => {
 }, 10);
 
 
-// Global variables
+// Clear arena scheduler
+system.runInterval(() => {
+    let countdown = 6;
 
-// Dictionary<VictimPlayerID, Dictionary<AttackerPlayerID, DamageAmount>>
-let playerDamages = {}
+    const intervalID = system.runInterval(() => {
+        if (countdown < 1) {
+            system.clearRun(intervalID);
+            clearArena();
+            return;
+        }
+        sendSubtitle(`\n\n§2Clearing arena in §e${countdown}§2...`, 0, 25, 30);
+        countdown--;
+    }, 20);
+
+}, 5 * 60 * TicksPerSecond);
+
+
+// Global variables
+const arenaCenterX = 20000;
+const arenaWidthX = 180;
+const arenaCenterZ = 20000;
+const arenaWidthZ = 180;
+const arenaBottomY = -64;
+const arenaHeightY = 25;
+
+let playerDamages = {};    // Dictionary<VictimPlayerID, Dictionary<AttackerPlayerID, DamageAmount>>
 
 
 log("[§4KitFFA§r]§a Addon loaded!");
